9187b706966db2da2d90b6de5bf15bf5499fc186
yalab <rudeboyjet@gmail.com>
2013-07-08 22:51:43 +0900

    Fixed knife.rb for knife-solo 0.3.

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,2 +1,6 @@
-data_bag_path "./data_bags"
 knife[:solo_path] = '/tmp/chef-solo'
+
+cookbook_path ["cookbooks", "site-cookbooks"]
+role_path     "roles"
+data_bag_path "data_bags"
+encrypted_data_bag_secret "encrypted_data_bag_secret"
