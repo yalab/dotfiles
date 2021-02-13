acc415405689598128ec015cf9d2d39bf6a42bbb
yalab <rudeboyjet@gmail.com>
2021-02-13 16:01:24 +0900

    remove chef

--- .chef/knife.rb
+++
@@ -1,9 +0,0 @@
-knife[:solo_path] = '/tmp/chef-solo'
-
-cookbook_path ["cookbooks", "site-cookbooks", "."]
-role_path     "roles"
-data_bag_path "data_bags"
-encrypted_data_bag_secret "encrypted_data_bag_secret"
-cookbook_license "mit"
-cookbook_copyright "oke-ya.inc"
-cookbook_email "yalab@oke-ya.com"
